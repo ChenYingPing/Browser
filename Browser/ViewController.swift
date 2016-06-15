46792ed2dfad9ac79e1038327cef11bd597fbb43
ChenYingPing <1417alk@sina.com>
2016-06-15 17:00:19 +0800

    Initial Commit

---
+++ Browser/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Browser
+//
+//  Created by Chen on 16/6/15.
+//  Copyright © 2016年 ChenYingPing. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
